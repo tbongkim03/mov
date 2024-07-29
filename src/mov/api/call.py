44e91250392f871e7486e6c2e19860521c2bd813
tbongkim03 <tbongkim03@gmial.com>
2024-07-29 14:37:28 +0900

    abc

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -4,26 +4,28 @@ import pandas as pd
 #추가 부분
 from datetime import datetime
 
-def save2df():
-    df = list2df()
+def save2df(load_dt='20120101'):
+
+    """airflow 호출 지점"""
+    df = list2df(load_dt)
     # df에 load_dt 컬럼 추가 (조회 일자 YYYYMMDD 형식으로)
     # 아래 파일 저장시 load_dt 기준으로 파티셔닝
-    now = datetime.now()
+    #now = datetime.now()
     #print(now)
-    date = now.strftime('%Y%m%d')
-    df['load_dt'] = date
+    #date = now.strftime('%Y%m%d')
+    df['load_dt'] = load_dt
     df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
     return df
 
 
-def list2df():
-    l = req2list()
+def list2df(load_dt='20120101'):
+    l = req2list(load_dt)
     df = pd.DataFrame(l)
     #print(df)
     return df
 
-def req2list() -> list:
-    _, data = req() # 생략할 때 사용
+def req2list(load_dt='20120101') -> list:
+    _, data = req(load_dt) # 생략할 때 사용
     l = data.get('boxOfficeResult').get('dailyBoxOfficeList') 
     #l = data['BoxOfficeResult']['dailyBoxOfficeList']
     #for i in l:
@@ -37,8 +39,8 @@ def get_key():
     key = os.getenv('MOVIE_API_KEY')
     return key
 
-def req(a='20230101'):
-    url = gen_url(a)
+def req(load_dt='20230101'):
+    url = gen_url(load_dt)
     r = requests.get(url)
     code = r.status_code
     data = r.json()
@@ -49,9 +51,9 @@ def req(a='20230101'):
     #print(data)
     return code, data
 
-def gen_url(dt="20120101"):
+def gen_url(load_dt='20120101'):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
-    url = f"{base_url}?key={key}&targetDt={dt}"
+    url = f"{base_url}?key={key}&targetDt={load_dt}"
 
     return url
--- tests/test_call.py
+++ tests/test_call.py
@@ -8,7 +8,10 @@ def test_비밀키숨기기():
 def test_유알엘테스트():
     url = gen_url()
     #print('2')
-    assert "http" in url
+    #assert "http" in url
+    url = gen_url('20241231')
+    print(url)
+    assert '20241231' in url
 
 def test_req():
     code, data = req()
@@ -37,8 +40,8 @@ def test_list2df():
     #print('5')
 
 def test_save2df():
-    df = save2df()
+    df = save2df(load_dt='20241231')
     print("\n")
     print(df)
     assert isinstance(df, pd.DataFrame)
-    assert 'load_dt' in df.columns
+    assert 'load_dt' in df.columns 
